f80fd325356c3f5aa242762861a8c320da19a0cd
Arthutstuts96 <oarthurlima1575@gmail.com>
2024-09-18 22:51:16 -0300

    Prototipacao do front end quase completa

---
+++ visualizarPerfilOutrosUsuarios.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/styles.css"/>
+    <title>Perfil de X | Gamyx</title>
+</head>
+<body>
+    <div class="container-tela-visualizar-perfis">
+        <header class="banner">
+            <img 
+                src="placeholder.jpg" 
+                alt="Banner do perfil do usuário X"
+                class="imagem-banner"
+            />
+        </header>
+        <main>
+            <section class="container-informacoes-perfil">
+                <img 
+                    src="./Projeto/GAMYX.png"
+                    alt="Imagem de perfil do usuário X"
+                    class="imagem-perfil"
+                />
+                <div class="informacoes-perfil">
+                    <h1>Nome do perfil</h1>
+                    <span>@arroba_do_usuario</span>
+                    <p class="descricao-usuario">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dicta doloribus molestiae voluptate numquam, totam eos, corporis illum maxime inventore qui? Distinctio voluptatem voluptatibus nisi cumque pariatur accusantium iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ea esse nesciunt culpa laboriosam dignissimos, quo repudiandae ut sapiente dolores odio soluta impedit veniam, explicabo alias nam quibusdam enim similique!</p>
+                    <div class="informacoes-perfil-icones">
+                        <span><i class="fa-regular fa-folder"></i> 42 projetos •</span>
+                        <span><i class="fa-solid fa-heart"></i> 2 Seguidores</span>
+                    </div>
+                </div>
+            </section>
+            <hr/>
+            <h1 class="titulo-projetos">Projetos</h1>
+            <section class="container-projetos">
+                <ul class="lista-projetos">
+                    <li class="item-projeto">
+                        <img 
+                            src=""
+                            alt="Imagem"
+                            class="imagem-projeto"
+                        />
+                    </li>
+                    <li class="item-projeto">
+                        <img 
+                            src=""
+                            alt="Imagem"
+                            class="imagem-projeto"
+                        />
+                    </li>
+                    <li class="item-projeto">
+                        <img 
+                            src=""
+                            alt="Imagem"
+                            class="imagem-projeto"
+                        />
+                    </li>
+                    <li class="item-projeto">
+                        <img 
+                            src=""
+                            alt="Imagem"
+                            class="imagem-projeto"
+                        />
+                    </li>
+                    <li class="item-projeto">
+                        <img 
+                            src=""
+                            alt="Imagem"
+                            class="imagem-projeto"
+                        />
+                    </li>
+                </ul>
+            </section>
+        </main>
+        <footer></footer>
+    </div>
+</body>
+</html>
+
