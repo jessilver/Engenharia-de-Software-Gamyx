8c602eb30ebf1777dc1c2815354f32ca27b6d49d
Arthutstuts96 <oarthurlima1575@gmail.com>
2024-10-15 21:54:57 -0300

    Configuração inicial e testes com o projeto

--- Projeto/src/routes.php
+++ Projeto/src/routes.php
@@ -7,5 +7,7 @@ $router->get('/', 'HomeController@index');
 $router->get('/perfil', 'viewProfileController@index');
 $router->get('/perfil/{id}', 'viewProfileController@other');
 
+$router->get('/projeto/{id}', 'viewProjectController@index');
+
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
 $router->get('/sobre', 'HomeController@sobre');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'f479a597632b55d9f293ae1e7e74a3df9a50ce52',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'f479a597632b55d9f293ae1e7e74a3df9a50ce52',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'clancats/hydrahon' => array(
+            'pretty_version' => 'v1.1.14',
+            'version' => '1.1.14.0',
+            'reference' => 'b1e1b2207ff57815a41e6832803cdc1d46e36f19',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../clancats/hydrahon',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
